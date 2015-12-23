18bf031e5fb2b56eeefd994e4a901b80e9acb6f5
JohnLance <a290873309@gmail.com>
2015-12-23 11:36:28 +0800

    update

--- hello.py
+++ hello.py
@@ -1,10 +1,14 @@
-from flasky import Flasky
+from flask import Flask, render_template
 
-app = Flasky(__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return '<h1>Hello World!<h1>'
+	return render_template('index.html')
+
+@app.route('/user/<name>')
+def user(name):
+	return render_template('user.html',name = name)
 
 if __name__ == '__main__':
 	app.run(debug = True)
