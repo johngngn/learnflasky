53812db93a388d6cbd44a4ec9aa205b295cae89e
JohnLance <a290873309@gmail.com>
2015-12-27 21:43:18 +0800

    update

--- hello.py
+++ hello.py
@@ -12,6 +12,7 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 from flask.ext.migrate import Migrate, MigrateCommand
 from flask.ext.mail import Mail, Message
+from threading import Thread
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -37,12 +38,19 @@ migrate = Migrate(app, db)
 mail = Mail(app)
 
 
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+
 def send_email(to, subject, template, **kwargs):
     msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject,
                   sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
     msg.body = render_template(template + '.txt', **kwargs)
     msg.html = render_template(template + '.html', **kwargs)
-    mail.send(msg)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+    return thr
 
 
 class Role(db.Model):
