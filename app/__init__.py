e2bf6099d68eef082d8a768fa01e403ab2b30261
JohnLance <a290873309@gmail.com>
2016-01-02 17:38:13 +0800

    update

--- app/__init__.py
+++ app/__init__.py
@@ -7,12 +7,17 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from config import config
 from flask.ext.mail import Mail
 from flask_debugtoolbar import DebugToolbarExtension
+from flask.ext.login import LoginManager
+
 
 bootstrap = Bootstrap()
 moment = Moment()
 db = SQLAlchemy()
 mail = Mail()
 toolbar = DebugToolbarExtension()
+login_manage = LoginManager()
+login_manage.session_protection = 'strong'
+login_manage.login_view = 'auth.login'
 
 
 def create_app(config_name):
@@ -30,8 +35,12 @@ def create_app(config_name):
     moment.init_app(app)
     db.init_app(app)
     toolbar.init_app(app)
+    login_manage.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     return app
--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
-from . import db
+from . import db, login_manage
+from flask.ext.login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
+
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -11,12 +13,13 @@ class Role(db.Model):
         return 'Role %r' % self.name
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
+    email = db.Column(db.String(64), unique=True, index=True)
 
     @property
     def password(self):
@@ -31,3 +34,8 @@ class User(db.Model):
 
     def __repr__(self):
         return 'User %r' % self.username
+
+
+@login_manage.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
