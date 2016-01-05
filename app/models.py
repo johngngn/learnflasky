51031d792c92d67cc16fc7c8c63316f6f0bc3d6c
JohnLance <a290873309@gmail.com>
2016-01-05 14:41:18 +0800

    update

--- app/models.py
+++ app/models.py
@@ -1,15 +1,47 @@
+#-*- coding:utf8 -*-
+#coding=utf-8
 from flask import current_app
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from . import db, login_manage
-from flask.ext.login import UserMixin
+from flask.ext.login import UserMixin, AnonymousUserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
+
+
+class Permissions:
+    FOLLOW = 0x01
+    COMMENT = 0x02
+    WRITE_ARTICLES = 0x04
+    MODERATE_COMMENTS = 0x08
+    ADMINISTER = 0x80
 
 
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
+    default = db.Column(db.Boolean, default=False, index=True)
+    permissions = db.Column(db.Integer)
     users = db.relationship('User', backref='role', lazy='dynamic')
+
+    #这里是利用循环字典来创建角色，这样可以方便以后扩展这个角色字典
+    #循环体用角色名称代入查询数据库是否已经存在，否的话用Role类来创建新对象，再加入数据库
+    @staticmethod
+    def insert_roles():
+        roles = {
+            'User': (Permissions.FOLLOW | Permissions.COMMENT |
+            Permissions.WRITE_ARTICLES, True),
+            'Moderator': (Permissions.FOLLOW | Permissions.COMMENT |
+            Permissions.WRITE_ARTICLES | Permissions.MODERATE_COMMENTS, False),
+            'Administrator': (0xff, False)
+        }
+        for r in roles:
+            role = Role.query.filter_by(name=r).first()
+            if role is None:
+                role = Role(name=r)
+            role.permissions = roles[r][0]
+            role.default = roles[r][1]
+            db.session.add(role)
+        db.session.commit()
 
     def __repr__(self):
         return 'Role %r' % self.name
@@ -24,6 +56,14 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(64), unique=True, index=True)
     confirmed = db.Column(db.Boolean, default=False)
 
+    def __init__(self, **kwargs):
+        super(User, self).__init__(**kwargs)
+        if self.role is None:
+            if self.email == current_app.config['FLASKY_ADMIN']:
+                self.role = Role.query.filter_by(permissions=0xff).first()
+            if self.role is None:
+                self.role = Role.query.filter_by(default=True).first()
+
     @property
     def password(self):
         raise AttributeError('password is not a readable attribute')
@@ -34,9 +74,6 @@ class User(UserMixin, db.Model):
 
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
-
-    def __repr__(self):
-        return 'User %r' % self.username
 
     def generate_confirmation_token(self, expiration=3600):
         s = Serializer(current_app.config['SECRET_KEY'], expiration)
@@ -91,6 +128,25 @@ class User(UserMixin, db.Model):
         db.session.add(self)
         return True
 
+    def can(self, permissions):
+        return self.role is not None and \
+            (self.role.permissions & permissions) == permissions
+
+    def is_administrator(self):
+        return self.can(Permissions.ADMINISTER)
+
+    def __repr__(self):
+        return 'User %r' % self.username
+
+
+class AnonymousUser(AnonymousUserMixin):
+    def can(self, permissions):
+        return False
+
+    def is_administrator(self):
+        return False
+
+login_manage.anonymous_user = AnonymousUser
 
 @login_manage.user_loader
 def load_user(user_id):
--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -1,6 +1,6 @@
 import unittest
 import time
-from app.models import User
+from app.models import User, Role, AnonymousUser, Permissions
 from app import db, create_app
 
 
@@ -105,3 +105,13 @@ class UserModelTestCase(unittest.TestCase):
         token = u2.generate_email_change_token('john@example.com')
         self.assertFalse(u2.change_email(token))
         self.assertTrue(u2.email == 'susan@example.com')
+
+    def test_roles_and_permissions(self):
+        Role.insert_roles()
+        u = User(email='john@example.com', password='cat')
+        self.assertTrue(u.can(Permissions.WRITE_ARTICLES))
+        self.assertFalse(u.can(Permissions.MODERATE_COMMENTS))
+
+    def test_anonymous_user(self):
+        u = AnonymousUser()
+        self.assertFalse(u.can(Permissions.FOLLOW))
