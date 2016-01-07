d269c993eae4bd5fa05b898edb34481ff32459bb
JohnLance <a290873309@gmail.com>
2016-01-07 15:26:35 +0800

    update

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 #-*- coding:utf8 -*-
 #coding=utf-8
-from flask import render_template, redirect, url_for, abort, flash
+from flask import render_template, redirect, url_for, flash
 from flask.ext.login import current_user, login_required
 from .forms import EditProfileForm, EditProfileAdminForm, PostForm
 from . import main
@@ -25,7 +25,8 @@ def index():
 @main.route('/user/<username>')
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    return render_template('user.html', user=user)
+    posts = user.posts.order_by(Post.timestamp.desc()).all()
+    return render_template('user.html', user=user, posts=posts)
 
 
 @main.route('/edit-profile', methods=['GET', 'POST'])
