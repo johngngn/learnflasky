56d1bcd44a78933abfcdc1282247fd6572442c5b
JohnLance <a290873309@gmail.com>
2016-01-08 23:48:16 +0800

    update

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 #-*- coding:utf8 -*-
 #coding=utf-8
-from flask import render_template, redirect, url_for, flash, request, current_app
+from flask import render_template, redirect, url_for, flash, request, current_app, abort
 from flask.ext.login import current_user, login_required
 from .forms import EditProfileForm, EditProfileAdminForm, PostForm
 from . import main
@@ -81,3 +81,20 @@ def edit_profile_admin(id):
 def post(id):
     post = Post.query.get_or_404(id)
     return render_template('post.html', posts=[post])
+
+
+@main.route('/edit/<int:id>', methods=['GET', 'POST'])
+@login_required
+def edit(id):
+    post = Post.query.get_or_404(id)
+    if current_user != post.author and \
+            not current_user.can(Permissions.ADMINISTER):
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.body = form.body.data
+        db.session.add(post)
+        flash('The post has been updated.')
+        return redirect(url_for('.post', id=post.id))
+    form.body.data = post.body
+    return render_template('edit_post.html', form=form)
