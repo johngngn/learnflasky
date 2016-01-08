30658a98b47fe1c640351c6577afc62294f8533d
JohnLance <a290873309@gmail.com>
2016-01-08 12:42:01 +0800

    update

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 #-*- coding:utf8 -*-
 #coding=utf-8
-from flask import render_template, redirect, url_for, flash
+from flask import render_template, redirect, url_for, flash, request, current_app
 from flask.ext.login import current_user, login_required
 from .forms import EditProfileForm, EditProfileAdminForm, PostForm
 from . import main
@@ -18,8 +18,13 @@ def index():
                     author=current_user._get_current_object())
         db.session.add(post)
         return redirect(url_for('.index'))
-    posts = Post.query.order_by(Post.timestamp.desc()).all()
-    return render_template('index.html', form=form, posts=posts)
+    page = request.args.get('page', 1, type=int)
+    pagination = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
+        error_out=False)
+    posts = pagination.items
+    return render_template('index.html', form=form, posts=posts,
+                           pagination=pagination)
 
 
 @main.route('/user/<username>')
